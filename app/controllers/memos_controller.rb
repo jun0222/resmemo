3966e3a50c7efca391e413ec3970d847906c8573
jun0222 <nextseason6@gmail.com>
2019-04-15 19:11:54 +0900

    add memos_controller

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,27 @@
+class MemosController < ApplicationController
+  def index
+    @memos = Memo.all
+  end
+
+  def new
+  end
+
+  def create
+    Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
+    redirect_to "/"
+  end
+
+  def destroy
+    memo = Memo.find(params["id"])
+    memo.destroy
+    redirect_to "/"
+  end
+
+  def edit
+    @memo = Memo.find(params["id"])
+  end
+
+  def update
+
+  end
+end
